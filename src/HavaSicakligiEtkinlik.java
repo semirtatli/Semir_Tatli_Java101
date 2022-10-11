17c43016c0efd95c4a6585f58905fefe3216fd0a
Semir Tatli <semirtatli@outlook.com>
2022-10-12 00:25:31 +0300

    Hava Sicakligi Etkinlik added

---
+++ src/HavaSicakligiEtkinlik.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class HavaSicakligiEtkinlik {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sıcaklık Giriniz: ");
+        int heat = input.nextInt();
+        
+        if (heat<5)
+            System.out.println("Kayak yapabilirsiniz");
+        else if (heat < 10)
+            System.out.println("Sinemaya gidebilirsiniz");
+        else if (heat >= 10 && heat <= 15) {
+            System.out.println("Sinemaya gidebilir veya Piknik yapabilirsiniz");
+        }
+        else if (heat < 25)
+            System.out.println("Piknik yapabilirsiniz");
+        else
+            System.out.println("Yüzmeye gidebilirsiniz");
+
+    }
+}

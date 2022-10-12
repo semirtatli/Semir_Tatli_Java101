952b2f23c6cdc6c919a2078a64994e4b48c5a4bc
Semir Tatli <semirtatli@outlook.com>
2022-10-12 12:04:55 +0300

    main changed

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("deneme");
+        int a = input.nextInt();
+        System.out.println(a);
     }
 }

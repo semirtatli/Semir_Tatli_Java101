1dee2302cf611c8e0426d3c2302c5aa58ccc3627
Semir Tatli <semirtatli@outlook.com>
2023-01-14 20:53:24 +0300

    Main class added to Mine Sweeper

--- src/mineSweeper/Main.java
+++ src/mineSweeper/Main.java
@@ -1,4 +1,8 @@
-package mineSweeper;
-
 public class Main {
+    //    mineSweeper mine = new mineSweeper(2, 3);
+    public static void main(String[] args) {
+        mineSweeper mine = new mineSweeper(4, 5);
+        mine.init();
+    }
 }
+
